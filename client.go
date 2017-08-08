08e25134c07a76b539952a719a3bb5baf20a018c
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-08-09 04:32:49 +0900

    Add error handling

--- client.go
+++ client.go
@@ -58,21 +58,38 @@ func (c *Client) getContext(ctx context.Context, endpoint *url.URL, query url.Va
 	}
 	defer res.Body.Close()
 
-	return ioutil.ReadAll(res.Body)
+	if response, err = ioutil.ReadAll(res.Body); err != nil {
+		return nil, err
+	}
+	if res.StatusCode == 200 {
+		return response, nil
+	}
+
+	var errors Errors
+
+	if err = json.Unmarshal(response, &errors); err != nil {
+		return nil, err
+	}
+	if len(errors.Errors) == 0 {
+		return nil, fmt.Errorf("error response is broken")
+	}
+
+	return nil, errors.Errors[0]
 }
 
-func (c *Client) Issues() ([]*Issue, error) {
-	return c.IssuesContext(context.Background())
+func (c *Client) Issues(query url.Values) ([]*Issue, error) {
+	return c.IssuesContext(context.Background(), query)
 }
 
-func (c *Client) IssuesContext(ctx context.Context) ([]*Issue, error) {
+func (c *Client) IssuesContext(ctx context.Context, query url.Values) ([]*Issue, error) {
 	var err error
 	var response []byte
 	var issues []*Issue
 	var path *url.URL
 
-	query := url.Values{}
-
+	if query == nil {
+		query = url.Values{}
+	}
 	if path, err = c.apiroot.Parse("./issues"); err != nil {
 		return nil, err
 	}
