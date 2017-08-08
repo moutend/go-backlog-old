cab8207e2a87207e9bce98f153193fc93e0af432
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-08-08 13:43:39 +0900

    Fix type of EstimatedHours and ActualHours

--- issue.go
+++ issue.go
@@ -14,18 +14,18 @@ type Issue struct {
 	Assignee       User      `json:"assignee"`
 	Category       []string  `json:"category"`
 	Versions       []string  `json:"versions"`
-	Milestone      string    `json:"milestone"`
+	Milestone      []string  `json:"milestone"`
 	StartDate      Date      `json:"startDate"`
 	DueDate        Date      `json:"dueDate"`
-	EstimatedHours int       `json:"estimatedHours"`
-	ActualHours    int       `json:"actualHours"`
+	EstimatedHours float64   `json:"estimatedHours"`
+	ActualHours    float64   `json:"actualHours"`
 	ParentIssueId  int       `json:"parentIssueId"`
 	CreatedUser    User      `json:"createdUser"`
 	Created        Date      `json:"created"`
 	UpdateUser     User      `json:"updatedUser"`
 	Updated        Date      `json:"updated"`
-	CustomFields   string    `json:"customFields"`
+	CustomFields   []string  `json:"customFields"`
 	Attachments    []string  `json:"attachments"`
 	SharedFiles    []string  `json:"sharedFiles"`
-	Stars          int       `json:"stars"`
+	Stars          []string  `json:"stars"`
 }
