b837a0f200762ce614eda08da744d3a049ad10fd
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-08-09 04:31:23 +0900

    Add Time method

--- date.go
+++ date.go
@@ -1,4 +1,14 @@
 package backlog
 
+import (
+	"time"
+)
+
 // Date represents the date string. The form of date is "2017-08-08T00:56:08Z".
 type Date string
+
+func (d Date) Time() time.Time {
+	t, _ := time.Parse(time.RFC3339, string(d))
+
+	return t
+}
