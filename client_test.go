3aebbd328104f10e2026843d46d902d5d74d3d94
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-08-10 07:58:56 +0900

    Add test

---
+++ client_test.go
@@ -0,0 +1,80 @@
+package backlog
+
+import (
+	"context"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+var (
+	client   *Client
+	testdata string
+)
+
+func setup() (*httptest.Server, error) {
+	testdata = filepath.Join("testdata", "api", "v2")
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		path := filepath.Join(
+			testdata,
+			strings.Replace(r.URL.Path, "/", string(os.PathSeparator), -1),
+			r.Method+".json")
+
+		file, err := ioutil.ReadFile(path)
+		if err != nil {
+			panic(path + " not found")
+		}
+
+		w.Write(file)
+		return
+	}))
+
+	root, err := url.Parse(server.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	client = &Client{
+		root: root,
+	}
+
+	return server, nil
+}
+
+func teardown(server *httptest.Server) error {
+	server.Close()
+
+	return nil
+}
+
+func TestMain(m *testing.M) {
+	server, err := setup()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	code := m.Run()
+
+	err = teardown(server)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	os.Exit(code)
+
+	return
+}
+
+func TestGetIssuesContext(t *testing.T) {
+	ctx := context.Background()
+	_, err := client.GetIssuesContext(ctx, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return
+}
