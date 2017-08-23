5c49fd746b9ab51df29c42cacbdd2d651461f8a8
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-08-23 14:02:44 +0900

    Add ChangeLog type

---
+++ changelog.go
@@ -0,0 +1,10 @@
+package backlog
+
+type ChangeLog struct {
+	Field            string `json:"field"`
+	NewValue         string `json:"newValue"`
+	OriginalValue    string `json:"originalValue"`
+	AttachmentInfo   string `json:"attachmentInfo"`
+	AttributeInfo    string `json:"attributeInfo"`
+	NotificationInfo string `json:"notificationInfo"`
+}
