d8a5f8ee30962853c124de25073c1f3b168e1f29
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-12-04 11:54:02 +0900

    Fix type of attachmentInfo

---
+++ attachmentinfo.go
@@ -0,0 +1,6 @@
+package backlog
+
+type AttachmentInfo struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
--- changelog.go
+++ changelog.go
@@ -4,7 +4,7 @@ type ChangeLog struct {
 	Field            string           `json:"field"`
 	NewValue         string           `json:"newValue"`
 	OriginalValue    string           `json:"originalValue"`
-	AttachmentInfo   string           `json:"attachmentInfo"`
+	AttachmentInfo   AttachmentInfo   `json:"attachmentInfo"`
 	AttributeInfo    string           `json:"attributeInfo"`
 	NotificationInfo NotificationInfo `json:"notificationInfo"`
 }
